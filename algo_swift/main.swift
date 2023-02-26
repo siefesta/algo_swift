a490f1d7350f8775379d7349b2e336ff3caca653
siefesta <siefesta@gmail.com>
2023-02-26 15:43:49 +0900

    1929

--- algo_swift/main.swift
+++ algo_swift/main.swift
@@ -2,14 +2,27 @@ import Foundation
 
 let inputs = readLine()!.split(separator: " ").map { Int($0)! }
 
-let a = inputs[0]
-let b = inputs[1]
-let c = inputs[2]
+let m = inputs[0]
+let n = inputs[1]
 
-if( b > c ) {
-    print("-1")
-} else {
-    let cost = c - b
-    print( Int(ceil(Double(a/cost))) + 1 )
+for i in m...n {
+    if(i % 2 == 0 || i == 1) {
+        continue
+    } else {
+        var start = 3
+        let end = Int(sqrt(Double(i)))
+        
+        while(true) {
+            if(start <= end) {
+                if(i % start == 0) {
+                    break
+                } else {
+                    start += 1
+                }
+            } else {
+                print("\(i)")
+                break
+            }
+        }
+    }
 }
-
