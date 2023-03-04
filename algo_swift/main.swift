c68eba1391caeb799ae1af272e552d9df4ab46f3
siefesta <siefesta@gmail.com>
2023-03-05 01:28:25 +0900

    0304

--- algo_swift/main.swift
+++ algo_swift/main.swift
@@ -1,43 +1,4 @@
 import Foundation
 
-var inputs = Array(readLine()!)
-
-var rear = 0
-var end = 0
-
-for i in 0..<inputs.count {
-    if(inputs[i] == " ") {
-        rear += 1
-    } else {
-        break
-    }
-}
-
-for j in (0..<inputs.count).reversed() {
-    if(inputs[j] == " ") {
-        end += 1
-    } else {
-        break
-    }
-}
-
-if (rear != 0) {
-    for _ in 0..<rear {
-        inputs.removeFirst()
-    }
-}
-
-if (end != 0) {
-    for _ in 0..<end {
-        inputs.removeLast()
-    }
-}
-
-var ans = 0
-for k in 0..<inputs.count {
-    if( inputs[k] == " ") {
-        ans += 1
-    }
-}
-
-print(ans + 1)
+var inputs = readLine()!.split(separator: " ")
+print(inputs.count)
