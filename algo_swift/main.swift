a94f1ab5596df56dc7e60a05d6f50e53e4914f5c
siefesta <siefesta@gmail.com>
2023-03-13 09:28:55 +0900

    0312

--- algo_swift/main.swift
+++ algo_swift/main.swift
@@ -1,19 +1,1 @@
 import Foundation
-
-let input = Array(readLine()!)
-var ans = true
-
-for i in 0..<(input.count/2) {
-            if(input[i] == input[input.count - i - 1]) {
-                continue
-            } else {
-                ans = false
-                break
-            }
-}
-
-if(ans) {
-    print("1")
-} else {
-    print("0")
-}
